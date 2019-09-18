6a251322bba9fde3e293f79b4dbe245d5324bcb7
serdg0 <sergio.diazg92@gmail.com>
2019-09-18 18:31:40 -0500

    added remember_token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,21 +3,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+    self.current_user = user
   end
+
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 
   def log_out
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,28 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token,:activation_token
+  attr_accessor :remember_token
+  before_create :create_remember_token
   validates :name,presence:true
   validates :email,presence:true
   has_secure_password
 
-  def User.new_token
+  def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-               BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
   end
 
-  def remember
-    #self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(User.new_token))
-  end
+
 
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+    self.remember_digest = remember_token
   end
 end
