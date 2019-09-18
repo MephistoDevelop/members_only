8cd959f3f7dfd8dcc257b01de0ef57b074f31c4e
Cristian <inesherald@gmail.com>
2019-09-18 12:11:11 -0500

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_213415) do
+ActiveRecord::Schema.define(version: 2019_09_17_214912) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
   end
 
 end
