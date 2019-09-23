b663528b08c5754886dff18ae050c823abcf5379
Cristian <inesherald@gmail.com>
2019-09-23 17:07:08 -0500

    Run rubocop fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return nil unless cookies.signed[:user_id]
-    @current_user ||= User.find_by(id: cookies.signed[:user_id])
+
+    @current_user||=User.find_by(id: cookies.signed[:user_id])
   end
 end
